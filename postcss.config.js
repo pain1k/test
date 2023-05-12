5c7ba0a3be855a51c8e6c078e60ddba8a9b6c986
pain1k <klikonnik@mail.ru>
2023-05-12 19:39:03 +0300

    delete mqpacker

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports={
     plugins:[
         require('autoprefixer'),
-        require('css-mqpacker')({
-            sort:true
-        }),
         require('cssnano')({
             preset:[
                 'default',{
